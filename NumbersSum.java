5ea32dd1cd6aca488478e6181dbdcdfafd73ce93
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-03-20 14:58:00 -0400

    Create NumbersSum.java

---
+++ NumbersSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class NumbersSum {
+    public static void main(String[] args) {
+        int userNum = 1;
+        int num;
+        int sum = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        num = input.nextInt();
+
+        while (userNum <= num){
+            System.out.println(userNum);
+            sum += userNum;
+            userNum++;
+        }
+        System.out.println("sUM :" + sum);
+    }
+}
