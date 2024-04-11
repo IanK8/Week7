23d75929c794dfe40de8106e3bf4b06f95809780
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-04-11 14:47:08 -0400

    Update Prompter.java

--- Prompter.java
+++ Prompter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+<<<<<<< HEAD
 
 public class Prompter {
     public static void main(String[] args) {
@@ -15,5 +16,25 @@ public class Prompter {
             }
         }
         System.out.println("You entered: " + userInput);
+=======
+public class Prompter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter minimum:");
+        int min = scanner.nextInt();
+        System.out.println("Enter maximum:");
+        int max = scanner.nextInt();
+        do {
+            System.out.println("Enter number between " + min + " and " + max + ":");
+            int userInput = scanner.nextInt();
+            if (userInput < min) {
+                if (userInput > max){
+                    System.out.println("Enter a number within range.");
+                }
+            }
+        }
+        System.out.println("You entered: " + userInput);
+        scanner.close();
+>>>>>>> 87085d56e7ad0832c9370024abc967799555ecec
     }
 }
